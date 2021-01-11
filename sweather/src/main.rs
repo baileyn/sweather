434cea324286c6571bd8084751434bf44d2509cc
Nicholas Bailey <nicholas.j.bailey91@gmail.com>
2021-01-10 23:19:32 -0500

    put together simple UI to ask for location

--- sweather/src/main.rs
+++ sweather/src/main.rs
@@ -1,14 +1,85 @@
 use dotenv::dotenv;
+use iced::{
+    text_input, Align, Column, Container, Element, HorizontalAlignment, Length, Sandbox, Settings,
+    Text, TextInput,
+};
 use weatherstack::blocking::*;
 
-fn main() {
+#[derive(Debug)]
+struct Sweather {
+    api: WeatherStack,
+    state: State,
+}
+
+#[derive(Debug, Default)]
+struct State {
+    location_input: text_input::State,
+    location_value: String,
+}
+
+#[derive(Debug, Clone)]
+enum Message {
+    InputChanged(String),
+    LookupWeather,
+}
+
+impl Sandbox for Sweather {
+    type Message = Message;
+
+    fn new() -> Self {
+        Self {
+            api: WeatherStack::new(
+                std::env::var("API_ACCESS_KEY").expect("Unable to obtain API_ACCESS_KEY"),
+            ),
+            state: State::default(),
+        }
+    }
+
+    fn title(&self) -> String {
+        "Sweather".into()
+    }
+
+    fn update(&mut self, message: Self::Message) {
+        match message {
+            Message::InputChanged(value) => {
+                self.state.location_value = value;
+            }
+            Message::LookupWeather => {
+                println!("Looking up weather for: {}", self.state.location_value);
+            }
+        }
+    }
+
+    fn view(&mut self) -> Element<'_, Self::Message> {
+        let title = Text::new("SWEATHER").size(50).color([0.5, 0.5, 0.5]);
+
+        let input = TextInput::new(
+            &mut self.state.location_input,
+            "Location",
+            &self.state.location_value,
+            Message::InputChanged,
+        )
+        .padding(15)
+        .on_submit(Message::LookupWeather);
+
+        let content = Column::new()
+            .width(Length::Units(800))
+            .align_items(Align::Center)
+            .spacing(20)
+            .push(title)
+            .push(input);
+
+        Container::new(content)
+            .width(Length::Fill)
+            .height(Length::Fill)
+            .padding(20)
+            .center_x()
+            .into()
+    }
+}
+
+fn main() -> iced::Result {
     dotenv().ok();
 
-    let api = WeatherStack::new(
-        std::env::var("API_ACCESS_KEY").expect("Expected API_ACCESS_KEY to be defined."),
-    );
-
-    let data = api.current("Oceana, WV".into());
-
-    println!("{:#?}", data);
+    Sweather::run(Settings::default())
 }
